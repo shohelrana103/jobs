550ccc255447eb0f8f335e9d3ef192b4e30302c6
unknown <sohel.rana@netmediamobile.com>
2023-06-04 13:55:06 +0600

    area model updated

--- common/models/area.py
+++ common/models/area.py
@@ -1,9 +1,15 @@
 from django.db import models
+from ..models.city import City
+from ..models.state import State
+from ..models.country import Country
 
 
 class Area(models.Model):
     id = models.BigAutoField(primary_key=True)
     area_name = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    state = models.ForeignKey(State, null=True, blank=True, on_delete=models.SET_NULL)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.area_name
