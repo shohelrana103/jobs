6615d94dcd45b7c0bcc9d5a1cced4022d3d1e10c
Md. Shohel Rana <sohel.rana@netmediamobile.com>
2023-06-24 22:26:45 +0600

    header

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -64,28 +64,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_METHODS = (
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-)
-
-CORS_ALLOW_HEADERS = (
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-)
 ROOT_URLCONF = 'jobportal.urls'
 
 TEMPLATES = [
