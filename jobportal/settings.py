86fc5d96c83e5505f63772e868cd45c2c4e214d9
Ubuntu <ubuntu@ip-172-31-95-22.ec2.internal>
2023-06-25 13:52:16 +0000

    push from server

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles/static/')
+    os.path.join(BASE_DIR, 'jobs/static/')
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
