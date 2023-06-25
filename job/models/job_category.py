2aa9a7c7a97953e6ffda0c41e4b4556d579c23fa
unknown <sohel.rana@netmediamobile.com>
2023-06-25 19:34:33 +0600

    category icon issue

--- job/models/job_category.py
+++ job/models/job_category.py
@@ -17,6 +17,15 @@ class JobCategory(models.Model):
 
 
 class JobCategorySerializer(serializers.ModelSerializer):
+    category_icon = serializers.SerializerMethodField()
+
     class Meta:
         model = JobCategory
         exclude = ('created_at', 'updated_at')
+
+    def get_category_icon(self, job_category):
+        if job_category.category_icon:
+            request = self.context.get('request')
+            return str(request.build_absolute_uri(job_category.category_icon.url))
+        else:
+            return None
