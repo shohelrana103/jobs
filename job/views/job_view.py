8638e5b4afedf82f4dd78aa8b88af16a8b1add26
unknown <sohel.rana@netmediamobile.com>
2023-07-19 12:35:10 +0600

    new api for stat and search

--- job/views/job_view.py
+++ job/views/job_view.py
@@ -1,6 +1,6 @@
 from rest_framework import status
 from django.http import JsonResponse
-from rest_framework.decorators import api_view, authentication_classes
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from ..models.job_category import JobCategory, JobCategorySerializer
@@ -10,6 +10,8 @@ from worker.models.worker import Worker
 from worker.models.job_application import JobApplication
 from worker.models.worker_shortlisted_job import WorkerShortListedJob
 from worker.models.job_favorite import WorkerFavoriteJob
+from company.models.company import Company
+from common.models.country import Country
 
 
 @api_view(['GET'])
@@ -40,7 +42,6 @@ def get_job_by_category(request, category_id):
         content['message'] = 'Job Category Not Found'
         return JsonResponse(content, status=status.HTTP_200_OK)
     jobs = Job.objects.filter(job_category=job_category)
-    print(jobs)
     serialized_jobs = JobSerializer(jobs, many=True, context={'request': request})
     content['status'] = 1
     content['message'] = 'Success'
@@ -224,4 +225,80 @@ def get_all_job_worker_id(request, worker_id):
     content['status'] = 1
     content['message'] = 'Success'
     content['jobs'] = send_data
+    return JsonResponse(content, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@authentication_classes((TokenAuthentication,))
+@permission_classes((IsAuthenticated,))
+def search_jobs(request):
+    content = {
+        'status': 0
+    }
+    jobs = Job.objects.all()
+    query_params = request.query_params
+    if "keyword" in query_params:
+        keyword = query_params['keyword']
+        jobs = jobs.filter(job_title__icontains=keyword)
+    if "country" in query_params:
+        country = query_params['country']
+        if country != 'all':
+            jobs = jobs.filter(country__id=country)
+    if "category" in query_params:
+        category = query_params['category']
+        if category != 'all':
+            jobs = jobs.filter(job_category__id=category)
+    send_data = []
+    if "worker_id" in query_params:
+        worker_id = query_params['worker_id']
+        try:
+            worker = Worker.objects.get(pk=worker_id)
+        except:
+            content['message'] = 'Worker not found'
+            return JsonResponse(content, status=status.HTTP_200_OK)
+        shortlisted_job_ids = list(WorkerShortListedJob.objects.filter(worker_id=worker).values_list('job_id', flat=True))
+        applied_job_ids = list(JobApplication.objects.filter(worker_id=worker).values_list('job_id', flat=True))
+        favorite_job_ids = list(WorkerFavoriteJob.objects.filter(worker_id=worker).values_list('job_id', flat=True))
+        for job in jobs:
+            serialized_job = JobSerializer(job, context={'request': request}).data
+            if job.id in applied_job_ids:
+                serialized_job.update({"is_applied": True})
+            else:
+                serialized_job.update({"is_applied": False})
+            if job.id in shortlisted_job_ids:
+                serialized_job.update({"is_shortlisted": True})
+            else:
+                serialized_job.update({"is_shortlisted": False})
+            if job.id in favorite_job_ids:
+                serialized_job.update({"is_favorite": True})
+            else:
+                serialized_job.update({"is_favorite": False})
+            send_data.append(serialized_job)
+    else:
+        send_data = JobSerializer(jobs, many=True, context={'request': request}).data
+    content['status'] = 1
+    content['message'] = 'Success'
+    content['jobs'] = send_data
+    return JsonResponse(content, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@authentication_classes((TokenAuthentication,))
+@permission_classes((IsAuthenticated,))
+def get_job_statistic(request):
+    content = {
+        'status': 0
+    }
+    total_companies = Company.objects.all().count()
+    total_worker = Worker.objects.all().count()
+    total_countries = Country.objects.all().count()
+    send_data = {
+        'total_companies': total_companies,
+        'total_worker': total_worker,
+        'total_countries': total_countries
+    }
+
+    content['status'] = 1
+    content['message'] = 'Success'
+    content['statistic'] = send_data
     return JsonResponse(content, status=status.HTTP_200_OK)
