1b0494a7256d3eb196058c8e2e6746e77c85cbe2
unknown <sohel.rana@netmediamobile.com>
2023-07-24 13:40:09 +0600

    updated

--- company/views/company_view.py
+++ company/views/company_view.py
@@ -244,7 +244,7 @@ def get_shortlisted_candidate(request, company_id, job_id):
         JobApplication.objects.filter(job_id=job, job_id__company=company, is_short_listed=True).values_list(
             'worker_id', flat=True))
     workers = Worker.objects.filter(pk__in=worker_ids)
-    serialized_jobs = WorkerSerializer(workers, many=True)
+    serialized_jobs = WorkerSerializer(workers, many=True, context={'request': request})
     content['status'] = 1
     content['message'] = 'Success'
     content['data'] = serialized_jobs.data
