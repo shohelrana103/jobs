90f3167ddac1f684d0a353da39e94d08b84c1193
Md. Shohel Rana <sohel.rana@netmediamobile.com>
2023-07-28 00:06:37 +0600

    email password change

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -162,6 +162,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'workersrus.ny@gmail.com'
-EMAIL_HOST_PASSWORD = 'jaozsvesyyihxiue'
+EMAIL_HOST_PASSWORD = 'woftzobwzqrggfzn'
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
