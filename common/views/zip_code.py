8a40d077de98c9ff53374b5744e9662929c59ae3
Md. Shohel Rana <sohel.rana@netmediamobile.com>
2023-09-17 21:19:04 +0600

    zip api

--- common/views/zip_code.py
+++ common/views/zip_code.py
@@ -13,13 +13,17 @@ def get_address_details(zip_code):
         ("codes", zip_code),
         ("country", "us"),
     )
-
-    response = requests.get('https://app.zipcodebase.com/api/v1/search', headers=headers, params=params);
-    # response = requests.get('https://app.zipcodebase.com/api/v1/search?apikey=365c3820-5565-11ee-87ee-7fca7f07c81d&codes=11021', headers=headers)
-    result = response.json()['results']
-    response_obj = result[str(zip_code)]
-    get_country, is_c = Country.objects.get_or_create(country_name='USA')
-    get_city, is_city = City.objects.get_or_create(city_name=response_obj[0]['city'], country=get_country)
-    get_state, is_state = State.objects.get_or_create(state_name=response_obj[0]['state'], country=get_country)
+    try:
+        response = requests.get('https://app.zipcodebase.com/api/v1/search', headers=headers, params=params);
+        # response = requests.get('https://app.zipcodebase.com/api/v1/search?apikey=365c3820-5565-11ee-87ee-7fca7f07c81d&codes=11021', headers=headers)
+        result = response.json()['results']
+        response_obj = result[str(zip_code)]
+        get_country, is_c = Country.objects.get_or_create(country_name='USA')
+        get_city, is_city = City.objects.get_or_create(city_name=response_obj[0]['city'], country=get_country)
+        get_state, is_state = State.objects.get_or_create(state_name=response_obj[0]['state'], country=get_country)
+    except:
+        get_country=None
+        get_city=None
+        get_state=None
     return get_country,get_city,get_state
 
